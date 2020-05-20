1d82ed4c21116aaa88b18d4eae3840294132f8c6
Dan <anne.leedan@gmail.com>
2020-05-20 11:28:29 -0400

    Improved UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,4 @@
 class User < ApplicationRecord
-  # before_create :set_default_avatar
-  # def set_default_avatar
-  #   self.avatar.attach("/assets/images/default_avatar.jpg")
-  # end
-
   has_one_attached :avatar
   has_many :comments
   # Include default devise modules. Others available are:
